0fa211bac0f983d6a3cb1dfcc35780729146b7ca
Hugo Adan <hugo@vauxoo.com>
2020-10-08 01:01:23 +0000

    [REM] deprecated modules now at v13.0

--- spc_account_invoice_line/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-#    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
-
-from . import models
--- spc_crm/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2016 ClearCorp
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import crm_lead, sale_config_settings, ir_model_data, sale_order
