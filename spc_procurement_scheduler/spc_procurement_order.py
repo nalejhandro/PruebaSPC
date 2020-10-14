9497d13616a553702060cc447f174516a769d0cf
Luis González <lgonzalez@vauxoo.com>
2020-10-14 03:02:11 +0000

    [REM] l10n_hn_company,spc_*: Remove Obsolete modules
    Only the main app is kept, because all other modules where never
    migrated from v8 (they're marked as not installable).
    
    The following modules are removed:
    - l10n_hn_company
    - spc_account_invoice_report
    - spc_employee_journal
    - spc_google_drive
    - spc_procurement_scheduler
    - spc_project_issue_search_attachments
    - spc_sale
    - spc_sale_order_purchase
    - spc_stock_intercompany_moves
    - spc_welcome_kits

--- spc_procurement_scheduler/spc_procurement_order.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, api
-
-class ProcurementComputeAll(models.TransientModel):
-
-    _inherit = 'procurement.order.compute.all'
-
-    def procure_calculation(self):
-        return super(ProcurementComputeAll, self).sudo().procure_calculation()
